912ab1310f95f8dd65dd5f82ad149432b6c11f24
Sergio Strazzacappa <strazzacappasergio@gmail.com>
2022-11-02 06:04:38 -0300

    2- Corregido install-req.sh

--- scripts/install-req.sh
+++ scripts/install-req.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
 
 sudo pacman -Sy
-sudo pacman -S bash binutils bison coreutils diffutils findutils gawk gcc grep gzip linux m4 make patch perl python sed tar texinfo xz
+sudo pacman -S bash 
+sudo pacman -S binutils
+sudo pacman -S bison
+sudo pacman -S coreutils
+sudo pacman -S diffutils
+sudo pacman -S findutils
+sudo pacman -S gawk
+sudo pacman -S gcc
+sudo pacman -S grep
+sudo pacman -S gzip
+sudo pacman -S linux
+sudo pacman -S m4
+sudo pacman -S make
+sudo pacman -S patch
+sudo pacman -S perl
+sudo pacman -S python
+sudo pacman -S sed
+sudo pacman -S tar
+sudo pacman -S texinfo
+sudo pacman -S xz
